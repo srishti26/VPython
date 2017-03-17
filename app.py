94e8473932a5394e2b27306742364ea5de314095
VIDUSHI <VIDUSHI@hp>
2017-03-17 18:39:29 +0530

    last

--- app.py
+++ app.py
@@ -11,21 +11,19 @@ app = Flask(__name__)
 @app.route('/api1', methods=['GET'])
 def api1():
     print "Hello I am in api1"
-    #imdb = Imdb()
-    imdb = Imdb(anonymize=True)
-    print imdb
-    print type(imdb)
+    imdb = Imdb()
     var1 = imdb.top_250()
-    print var1
     """var2 = imdb.search_for_title("FightClub")
     print var2
     #req = request.var1"""
     return jsonify(var1)
-    return render_template('index.html')
+    
 
 @app.route('/', methods=['GET'])
 def index():
     print "Hello I am in index"
     return render_template('index.html')
+    
+    
 if __name__ == "__main__":
     app.run()
