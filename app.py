742472d8391d6d42629b2b8d803b8a4553a371ac
VIDUSHI <VIDUSHI@hp>
2017-03-17 17:54:19 +0530

    final comit

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
+import os
+
 from flask import Flask
+from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
+from flask import request
+from imdbpie import Imdb
+
+
 app = Flask(__name__)
-@app.route('/')
+@app.route('/api1', methods=['GET'])
+def api1():
+    print "Hello I am in api1"
+    #imdb = Imdb()
+    imdb = Imdb(anonymize=True)
+    print imdb
+    print type(imdb)
+    var1 = imdb.top_250()
+    print var1
+    """var2 = imdb.search_for_title("FightClub")
+    print var2
+    #req = request.var1"""
+    return jsonify(var1)
+    return render_template('index.html')
+
+@app.route('/', methods=['GET'])
 def index():
-    return 'Yo, its working!'
+    print "Hello I am in index"
+    return render_template('index.html')
 if __name__ == "__main__":
     app.run()
